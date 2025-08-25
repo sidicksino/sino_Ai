fe8d100726078bc32a6294d3b3640beed37fa2b6
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-25 02:39:55 +0200

    Add sideBar and adding chat box

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,11 +3,13 @@ import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 import moment from 'moment'
 
-const SideBar = () => {
+const SideBar = ({ isMenuOpen, setIsMenuOpen }) => {
     const { chats, setSelectedChat, theme, setTheme, user, navigate } = useAppContext()
     const [search, setSearch] = useState('')
   return (
-    <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1'> 
+    <div className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-
+    [#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl 
+    transition-all duration-500 left-0 z-1 ${!isMenuOpen && 'max-md:-translate-x-full'}`}> 
       {/* Logo */}
       <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="" className='w-full max-w-48'/>
 
@@ -28,7 +30,7 @@ const SideBar = () => {
             chats.filter((chat) => chat.messages[0] ? chat.messages[0]?.content.
             toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().
             includes(search.toLowerCase())).map((chat)=>(
-                <div key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
+                <div onClick={()=>{navigate('/'); setSelectedChat(chat), setIsMenuOpen(false)}} key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
                     <div>
                         <p className='truncate w-full'>
                             {chat.messages.length > 0 ? chat.messages[0].content.
@@ -43,19 +45,19 @@ const SideBar = () => {
       </div>
 
       {/* Community images */}
-      <div onClick={()=> {navigate('/comminity')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+      <div onClick={()=> {navigate('/community'); setIsMenuOpen(false)}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
         <img src={assets.gallery_icon} className='w-4.5 not-dark:invert' alt="" />
         <div className='flex flex-col text-sm'>
-            <p>Comminity Images</p>
+            <p>community Images</p>
         </div>
       </div>
 
       {/* Credit Purchases Option */}
-      <div onClick={()=> {navigate('/credits')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+      <div onClick={()=> {navigate('/credits'); setIsMenuOpen(false)}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
         <img src={assets.diamond_icon} className='w-4.5 dark:invert' alt="" />
         <div className='flex flex-col text-sm'>
             <p>Credits : {user?.credits}</p>
-            <p className='text-sm text-gray-400'>Purchase creadits to use SINO Ai</p>
+            <p className='text-sm text-gray-400'>Purchase credits to use SINO Ai</p>
         </div>
       </div>
 
@@ -81,7 +83,7 @@ const SideBar = () => {
         {user && <img src={assets.logout_icon} className='h-5 cursor-pointer hidden not-dark:invert group-hover:block'/> }
       </div>
 
-      <img src={assets.close_icon} className='absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert' alt="" />
+      <img src={assets.close_icon} onClick={()=> setIsMenuOpen(false)} className='absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert' alt="" />
     </div>
   )
 }
